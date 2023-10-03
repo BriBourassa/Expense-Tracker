af6bb4ebd80280aa9fa3b8d1fcc9afac4370df9e
BriBourassa <111149043+BriBourassa@users.noreply.github.com>
2023-10-03 13:58:38 -0600

    Add NewExpense and styling

---
+++ src/Components/NewExpense/NewExpense.jsx
@@ -0,0 +1,12 @@
+import Form from "../Form/Form";
+import './NewExpense.css';
+
+const NewExpense = () => {
+    return (
+        <div className="new-expense">
+            <Form />
+        </div>
+    )
+}
+
+export default NewExpense;
